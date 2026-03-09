103116221d754c9f87d187307bb1514e20428538
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2026-03-09 17:40:14 +0100

    chore: define `Asset::is_same` based on vault key

--- crates/miden-protocol/src/asset/mod.rs
+++ crates/miden-protocol/src/asset/mod.rs
@@ -124,16 +124,9 @@ impl Asset {
 
     /// Returns true if this asset is the same as the specified asset.
     ///
-    /// Two assets are defined to be the same if:
-    /// - For fungible assets, if they were issued by the same faucet.
-    /// - For non-fungible assets, if the assets are identical.
+    /// Two assets are defined to be the same if their vault keys match
     pub fn is_same(&self, other: &Self) -> bool {
-        use Asset::*;
-        match (self, other) {
-            (Fungible(l), Fungible(r)) => l.is_from_same_faucet(r),
-            (NonFungible(l), NonFungible(r)) => l == r,
-            _ => false,
-        }
+        self.vault_key() == other.vault_key()
     }
 
     /// Returns true if this asset is a fungible asset.
