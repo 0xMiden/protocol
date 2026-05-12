walnuthq/miden-base

fix(protocol): encode note metadata v1 as one (#2898)
* fix(protocol): encode note metadata v1 as one

* fix(protocol): import ToString in metadata tests

* docs(protocol): clarify account id version checks

---------

Co-authored-by: Bobbin Threadbare <43513081+bobbinth@users.noreply.github.com>

crates/miden-protocol/src/note/metadata.rs
@@ -85,7 +85,7 @@ impl NoteMetadata {
     ///
     /// If we make this public, we may want to instead consider introducing a `NoteMetadataVersion`
     /// struct, similar to `AccountIdVersion`.
-    const VERSION_1: u8 = 0;
+    const VERSION_1: u8 = 1;
 
     // CONSTRUCTORS
     // --------------------------------------------------------------------------------------------
@@ -452,6 +452,8 @@ fn unmerge_attachment_kind_scheme(
 #[cfg(test)]
 mod tests {
 
+    use alloc::string::ToString;
+
     use super::*;
     use crate::note::NoteAttachmentScheme;
     use crate::testing::account_id::ACCOUNT_ID_MAX_ONES;
@@ -482,5 +484,30 @@ mod tests {
         assert_eq!(header, metadata.to_header());
 
         Ok(())
+    }
+
+    #[test]
+    fn note_metadata_header_encodes_v1_as_one() {
+        let sender = AccountId::try_from(ACCOUNT_ID_MAX_ONES).unwrap();
+        let metadata = NoteMetadata::new(sender, NoteType::Private);
+
+        let header = metadata.to_header_word();
+        let version = header[0].as_canonical_u64() & 0b1111;
+
+        assert_eq!(version, NoteMetadata::VERSION_1 as u64);
+        assert_eq!(version, 1);
+    }
+
+    #[test]
+    fn note_metadata_header_rejects_zero_version() {
+        let sender = AccountId::try_from(ACCOUNT_ID_MAX_ONES).unwrap();
+        let metadata = NoteMetadata::new(sender, NoteType::Private);
+        let mut header = metadata.to_header_word();
+
+        let header0 = header[0].as_canonical_u64() & !0b1111;
+        header[0] = Felt::try_from(header0).expect("header should still be a valid felt");
+
+        let err = NoteMetadataHeader::try_from(header).expect_err("version 0 should be rejected");
+        assert!(err.to_string().contains("unsupported note metadata version 0"), "{err}");
     }
 }

0xMiden/protocol

crates/miden-protocol/src/note/metadata.rs
@@ -166,7 +166,7 @@ impl NoteMetadata {
     ///
     /// If we make this public, we may want to instead consider introducing a `NoteMetadataVersion`
     /// struct, similar to `AccountIdVersion`.
-    const VERSION_1: u8 = 0;
+    const VERSION_1: u8 = 1;
 
     // CONSTRUCTORS
     // --------------------------------------------------------------------------------------------
@@ -371,6 +371,8 @@ fn merge_schemes(headers: [NoteAttachmentHeader; NoteAttachments::MAX_COUNT]) ->
 #[cfg(test)]
 mod tests {
 
+    use alloc::string::ToString;
+
     use super::*;
     use crate::note::{NoteAttachment, NoteAttachmentScheme};
     use crate::testing::account_id::ACCOUNT_ID_MAX_ONES;
@@ -442,5 +444,18 @@ mod tests {
         assert_eq!(roundtripped, metadata);
 
         Ok(())
+    }
+
+    #[test]
+    fn note_metadata_header_encodes_v1_as_one() {
+        let sender = AccountId::try_from(ACCOUNT_ID_MAX_ONES).unwrap();
+        let metadata = PartialNoteMetadata::new(sender, NoteType::Private);
+        let metadata = NoteMetadata::new(metadata, &NoteAttachments::default());
+
+        let metadata = metadata.to_metadata_word();
+        let version = metadata[0].as_canonical_u64() & 0b1111;
+
+        assert_eq!(version, NoteMetadata::VERSION_1 as u64);
+        assert_eq!(version, 1);
     }
 }