bf4a12d5ea1f5613c2c8854b308896cb25e7291f
onurinanc <e191322@metu.edu.tr>
2026-05-12 12:08:53 +0200

    fmt

--- crates/miden-testing/tests/scripts/send_note.rs
+++ crates/miden-testing/tests/scripts/send_note.rs
@@ -155,8 +155,8 @@ async fn test_send_note_script_fungible_faucet() -> anyhow::Result<()> {
 
     let tag = NoteTag::with_account_target(sender_fungible_faucet_account.id());
     let attachment = NoteAttachment::with_word(NoteAttachmentScheme::new(100)?, Word::empty());
-    let metadata = NoteMetadata::new(sender_fungible_faucet_account.id(), NoteType::Public)
-        .with_tag(tag);
+    let metadata =
+        NoteMetadata::new(sender_fungible_faucet_account.id(), NoteType::Public).with_tag(tag);
     let assets = NoteAssets::new(vec![Asset::Fungible(
         FungibleAsset::new(sender_fungible_faucet_account.id(), 10).unwrap(),
     )])?;
