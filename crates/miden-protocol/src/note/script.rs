walnuthq/miden-base

feat(protocol): add `NoteScriptRoot` newtype (#2851)
* feat(protocol): add NoteScriptRoot newtype

* docs: add changelog entry for NoteScriptRoot

* refactor(protocol): drop NoteScriptRoot::as_inner, unify on Word::from / into

Addresses PR #2851 review: prefer the existing Into<Word> conversion over a
bespoke as_inner accessor. Callsites that need a Word now use Word::from(...)
or .into() depending on whether the surrounding expression provides a target
type.

crates/miden-protocol/src/note/script.rs
@@ -1,10 +1,11 @@
-use alloc::string::ToString;
+use alloc::string::{String, ToString};
 use alloc::sync::Arc;
 use alloc::vec::Vec;
 use core::fmt::Display;
 use core::num::TryFromIntError;
 
 use miden_core::mast::MastNodeExt;
+use miden_crypto_derive::WordWrapper;
 use miden_mast_package::Package;
 
 use super::Felt;
@@ -23,6 +24,42 @@ use crate::{PrettyPrint, Word};
 
 /// The attribute name used to mark the entrypoint procedure in a note script library.
 const NOTE_SCRIPT_ATTRIBUTE: &str = "note_script";
+
+// NOTE SCRIPT ROOT
+// ================================================================================================
+
+/// The MAST root of a [`NoteScript`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, WordWrapper)]
+pub struct NoteScriptRoot(Word);
+
+impl From<NoteScriptRoot> for Word {
+    fn from(root: NoteScriptRoot) -> Self {
+        root.0
+    }
+}
+
+impl Display for NoteScriptRoot {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+impl Serializable for NoteScriptRoot {
+    fn write_into<W: ByteWriter>(&self, target: &mut W) {
+        target.write(self.0);
+    }
+
+    fn get_size_hint(&self) -> usize {
+        self.0.get_size_hint()
+    }
+}
+
+impl Deserializable for NoteScriptRoot {
+    fn read_from<R: ByteReader>(source: &mut R) -> Result<Self, DeserializationError> {
+        let word: Word = source.read()?;
+        Ok(Self::from_raw(word))
+    }
+}
 
 // NOTE SCRIPT
 // ================================================================================================
@@ -161,8 +198,8 @@ impl NoteScript {
     // --------------------------------------------------------------------------------------------
 
     /// Returns the commitment of this note script (i.e., the script's MAST root).
-    pub fn root(&self) -> Word {
-        self.mast[self.entrypoint].digest()
+    pub fn root(&self) -> NoteScriptRoot {
+        NoteScriptRoot::from_raw(self.mast[self.entrypoint].digest())
     }
 
     /// Returns a reference to the [MastForest] backing this note script.

0xMiden/protocol

crates/miden-protocol/src/note/script.rs
@@ -1,4 +1,4 @@
-use alloc::string::ToString;
+use alloc::string::{String, ToString};
 use alloc::sync::Arc;
 use alloc::vec::Vec;
 use core::fmt::Display;
@@ -6,6 +6,7 @@ use core::num::TryFromIntError;
 
 use miden_core::mast::MastNodeExt;
 use miden_mast_package::{MastArtifact, Package};
+use miden_protocol_macros::WordWrapper;
 
 use super::Felt;
 use crate::assembly::mast::{ExternalNodeBuilder, MastForest, MastForestContributor, MastNodeId};
@@ -23,6 +24,42 @@ use crate::{PrettyPrint, Word};
 
 /// The attribute name used to mark the entrypoint procedure in a note script library.
 const NOTE_SCRIPT_ATTRIBUTE: &str = "note_script";
+
+// NOTE SCRIPT ROOT
+// ================================================================================================
+
+/// The MAST root of a [`NoteScript`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, WordWrapper)]
+pub struct NoteScriptRoot(Word);
+
+impl From<NoteScriptRoot> for Word {
+    fn from(root: NoteScriptRoot) -> Self {
+        root.0
+    }
+}
+
+impl Display for NoteScriptRoot {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+impl Serializable for NoteScriptRoot {
+    fn write_into<W: ByteWriter>(&self, target: &mut W) {
+        target.write(self.0);
+    }
+
+    fn get_size_hint(&self) -> usize {
+        self.0.get_size_hint()
+    }
+}
+
+impl Deserializable for NoteScriptRoot {
+    fn read_from<R: ByteReader>(source: &mut R) -> Result<Self, DeserializationError> {
+        let word: Word = source.read()?;
+        Ok(Self::from_raw(word))
+    }
+}
 
 // NOTE SCRIPT
 // ================================================================================================
@@ -173,8 +210,8 @@ impl NoteScript {
     // --------------------------------------------------------------------------------------------
 
     /// Returns the commitment of this note script (i.e., the script's MAST root).
-    pub fn root(&self) -> Word {
-        self.mast[self.entrypoint].digest()
+    pub fn root(&self) -> NoteScriptRoot {
+        NoteScriptRoot::from_raw(self.mast[self.entrypoint].digest())
     }
 
     /// Returns a reference to the [MastForest] backing this note script.